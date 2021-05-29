ef0aa1008766f645004ec179ba34f4ee95e31119
Madan Naik <48375643+madannaik@users.noreply.github.com>
2021-05-29 17:34:48 +0530

    ad

--- src/API/graph.js
+++ src/API/graph.js
@@ -6,7 +6,7 @@ import { gethtmldata } from "./scotch";
 const token = '4a2a4c1372879d38c40f98bf3d447273';
 export const graph =  async () => {
     const url  = "https://ww1.animesimple.com/";
-    const webdata = await axios.get(`http://api.scrapeup.com/?api_key=${token}&url=${url}`);
+    const webdata = await axios.get(`https://api.scrapeup.com/?api_key=${token}&url=${url}`);
     const og = webdata.data
     const array = gethtmldata(og);
     return array;
